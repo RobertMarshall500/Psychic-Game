e98e572550ea731fdf4e651843d6cb7273414ed4
Robert Marshall <robertmarshall500@gmail.com>
2018-09-11 17:45:14 -0400

    pyschic game pseudo game

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+var thoughts = ["cars","money","fashion"]
+var losses;
+var wins;
+var guessesleft;
+var guessessofar;
+var startgame = function (){ thoughts = chooserandomthought();
+
+}
+var chooserandomthought = function (thoughts = ,"cars", "money", "fashion") {
+
+}
+var checkguess = function (wins) { 
+return wins.indexof (thoughts)>-1 
+}
+var handlewins = function (wins) {
+    if (checkguess (thoughts)){wins}
+    else (losses)
+}
+var tallyguessesleft = function (guessesleft) {
+    math.
+}
+{psuedo code
+    i need to link javascript correctly. i need to make a fucntion to keep a tally for guesses left and guesses so far.i need to make a function for enetering a word on the keyboard and link it with variable function to checkguess if game is won or lost. i need to make a var function to to reset the game.
+}
