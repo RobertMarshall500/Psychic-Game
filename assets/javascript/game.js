0a00ccb45939790255f0bb3342c7311f9389d8cb
Robert Marshall <robertmarshall500@gmail.com>
2018-10-16 13:44:53 -0400

    pyschcic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,58 @@
-var thoughts = ["cars","money","fashion"]
-var losses;
-var wins;
-var guessesleft;
-var guessessofar;
-var startgame = function (){ thoughts = chooserandomthought();
 
-}
-var chooserandomthought = function (thoughts = ,"cars", "money", "fashion") {
+var computerchoice = ["r", "p", "s"];
 
-}
-var checkguess = function (wins) { 
-return wins.indexof (thoughts)>-1 
-}
-var handlewins = function (wins) {
-    if (checkguess (thoughts)){wins}
-    else (losses)
-}
-var tallyguessesleft = function (guessesleft) {
-    math.
-}
-{psuedo code
-    i need to link javascript correctly. i need to make a fucntion to keep a tally for guesses left and guesses so far.i need to make a function for enetering a word on the keyboard and link it with variable function to checkguess if game is won or lost. i need to make a var function to to reset the game.
-}
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var tiesText = document.getElementById("ties-text");
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+  // Determines which key was pressed.
+  var userGuess = event.key;
+
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerGuess = computerChoices[Math.floor(Math.random() * guess.length)];
+
+  // Reworked our code from last step to use "else if" instead of lots of if statements.
+
+  // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+  if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+
+    if ((userGuess === "r") && (computerGuess === "s")) {
+      wins++;
+    } else if ((userGuess === "r") && (computerGuess === "p")) {
+      losses++;
+    } else if ((userGuess === "s") && (computerGuess === "r")) {
+      losses++;
+    } else if ((userGuess === "s") && (computerGuess === "p")) {
+      wins++;
+    } else if ((userGuess === "p") && (computerGuess === "r")) {
+      wins++;
+    } else if ((userGuess === "p") && (computerGuess === "s")) {
+      losses++;
+    } else if (userGuess === computerGuess) {
+      ties++;
+    }
+
+    // Hide the directions
+    directionsText.textContent = "";
+
+    // Display the user and computer guesses, and wins/losses/ties.
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    tiesText.textContent = "ties: " + ties;
+  }
+};
